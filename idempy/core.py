3963fbf74190e4f3b10b91f089b2c0aefd68c45f
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-14 11:26:46 +0000

    adding core boiler

--- idempy/core.py
+++ idempy/core.py
@@ -1,2 +1,21 @@
 from typing import Any
+import sys
+import os
+from datetime import datetime
+from idempy.models import IdempotencyKey
 
+
+
+DEFAULT_SETTINGS = {
+    'idempy_key_prefix': 'idempotency_key_',
+    'IdempotencyKey': IdempotencyKey,
+    'datetime_class': datetime,
+}
+
+class IdempotencyManager: 
+    def __init__(self, setttings: dict = DEFAULT_SETTINGS) -> None:
+        self.settings = settings
+        self.idempotency_key_prefix = settings.get('idempy_key_prefix', 'idempotency_key_')
+
+    def generate_key(self, IdempotencyKey: IdempotencyKey) -> str:
+        return f"{self.idempotency_key_prefix}{IdempotencyKey.key}"
